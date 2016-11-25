f2db8de4be0388e84a1d746a535aff62ae331b85
Reggie Mack <thegraphixninja@gmail.com>
2016-11-25 17:15:03 -0600

    Add js file
    Add js file

---
+++ js/widget.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+(function(){
+"use strict";
+    
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+          var employees = JSON.parse(xhr.responseText);
+          console.log(typeof employees);
+      }  
+    };
+    xhr.open('GET', 'data/employees.json');
+    xhr.send();
+    
+})();
