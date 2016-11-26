c48b713cb0d4510eeca9b5e3825855422d706c45
Reggie Mack <thegraphixninja@gmail.com>
2016-11-25 19:36:53 -0600

    Parse JSON Data
    Parse JSON Data and put in HTML

--- js/widget.js
+++ js/widget.js
@@ -6,7 +6,18 @@
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4) {
           var employees = JSON.parse(xhr.responseText);
-          console.log(typeof employees);
+          var statusHTML = '<ul class="bulleted">';
+          for (var i = 0; i < employees.length; i += 1) {
+              if (employees[i].inoffice === true) {
+                  statusHTML += '<li class="in">';
+              } else {
+                  statusHTML += '<li class="out">';
+              }
+              statusHTML += employees[i].name;
+              statusHTML += '</li>';
+          }
+          statusHTML += '</ul>';
+          document.getElementById('employeeList').innerHTML = statusHTML;
       }  
     };
     xhr.open('GET', 'data/employees.json');
